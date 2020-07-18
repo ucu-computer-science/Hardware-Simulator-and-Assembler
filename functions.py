d1e2153093e72a9c431bc1c56f5ac516865159d9
Daria Omelkina <odasha2001@gmail.com>
2020-07-18 17:27:25 +0300

    Create new functions and modify old behaviour in the functions dictionary

--- functions.py
+++ functions.py
@@ -1,56 +1,60 @@
-def load(*operands):
+def load(operands):
     pass
 
 
-def store(*operands):
+def store(operands):
     pass
 
 
-def mov_low1(*operands):
+def mov_low1(operands):
 	operands[0]._state = b"00000000" + operands[1]
 
 
-def mov_low2(*operands):
+def mov_low2(operands):
 	operands[0]._state = b"00000000" + operands[1]
 
 
-def mov_high1(*operands):
+def mov_high1(operands):
     operands[0]._state = operands[1] + operands[0][8:]
 
 
-def mov_high2(*operands):
+def mov_high2(operands):
     operands[0]._state = operands[1] + operands[0][8:]
 
 
-def mov(*operands):
-    operands[0]._state = operands[1]._state
+def mov(operands):
+    operands[0]._state = operands[1]
 
 
-def push(reg):
+def push(operands):
     pass
 
 
-def pop(reg):
+def pop(operands):
     pass
 
 
-def add(reg1, reg2, reg3):
-    reg1._state = reg2._state + reg3._state
+def add(operands):
+    operands[0]._state = bin(int(perands[1]._state) + int(operands[2]._state))
 
 
-def sub(reg1, reg2, reg3):
-    reg1._state = reg2._state - reg3._state
+def sub(operands):
+    operands[0]._state = bin(int(operands[1]._state) - int(operands[2]._state))
 
 
-def mul(reg1, reg2, reg3):
-    reg1._state = reg2._state * reg3._state
+def mul(operands):
+    operands[0]._state = bin(int(operands[1]._state) * int(operands[2]._state))
 
 
-def div(reg1, reg2, reg3):
-    reg1._state = reg2._state / reg3._state
+def div(operands):
+    operands[0]._state = bin(int(operands[1]._state) / int(operands[2]._state))
 
 
+def and(operands):
 
-fucnctions_dictionary = {"load":load, "mov_low1":mov_low1,
+
+functions_dictionary = {"load":load, "mov_low1":mov_low1,
                         "mov_low2":mov_low2, "mov_high1":mov_high1,
-                        "mov_high2":mov_high2, "mov":mov, }
+                        "mov_high2":mov_high2, "mov":mov,
+                        "push":push, "pop":pop, "add":add,
+                        "sub":sub, "mul":push, "div":div}
