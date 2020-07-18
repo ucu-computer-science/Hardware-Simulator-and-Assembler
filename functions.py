c9e2758d52424ce3398fae8f55de7b25b87b5747
Daria Omelkina <odasha2001@gmail.com>
2020-07-18 17:14:22 +0300

    Get functions dictionary functionality up to date with bytes usage

--- functions.py
+++ functions.py
@@ -1,29 +1,29 @@
-def load(reg, memreg):
+def load(*operands):
     pass
 
 
-def store(memreg, reg):
+def store(*operands):
     pass
 
 
-def mov_low1(reg, imm):
-	reg._state = b"00000000" + imm
+def mov_low1(*operands):
+	operands[0]._state = b"00000000" + operands[1]
 
 
-def mov_low2(reg, imm):
-	reg._state = b"00000000" + imm
+def mov_low2(*operands):
+	operands[0]._state = b"00000000" + operands[1]
 
 
-def mov_high1(reg, imm):
-    reg._state = imm + reg[8:]
+def mov_high1(*operands):
+    operands[0]._state = operands[1] + operands[0][8:]
 
 
-def mov_high2(reg, imm):
-    reg._state = imm + reg[8:]
+def mov_high2(*operands):
+    operands[0]._state = operands[1] + operands[0][8:]
 
 
-def mov(reg1, reg2):
-    reg1._state = reg2._state
+def mov(*operands):
+    operands[0]._state = operands[1]._state
 
 
 def push(reg):
@@ -38,9 +38,19 @@ def add(reg1, reg2, reg3):
     reg1._state = reg2._state + reg3._state
 
 
-# def
+def sub(reg1, reg2, reg3):
+    reg1._state = reg2._state - reg3._state
 
 
-fucnctions_dictionary = {"load":load(), "mov_low1":mov_low1(),
-                        "mov_low2":mov_low2(), "mov_high1":mov_high1(),
-                        "mov_high2":mov_high2(), "mov":mov(), }
+def mul(reg1, reg2, reg3):
+    reg1._state = reg2._state * reg3._state
+
+
+def div(reg1, reg2, reg3):
+    reg1._state = reg2._state / reg3._state
+
+
+
+fucnctions_dictionary = {"load":load, "mov_low1":mov_low1,
+                        "mov_low2":mov_low2, "mov_high1":mov_high1,
+                        "mov_high2":mov_high2, "mov":mov, }
