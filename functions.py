08069952c8a1939d246c75b49bc66a2040c3c451
Daria Omelkina <odasha2001@gmail.com>
2020-07-18 17:07:43 +0300

    Update functions dictionary

--- functions.py
+++ functions.py
@@ -1,31 +1,46 @@
-# def load(reg, memreg):
-#     reg._state = memreg
-#
-# def store(memreg, reg):
-# 	memreg = reg
+def load(reg, memreg):
+    pass
+
+
+def store(memreg, reg):
+    pass
+
 
 def mov_low1(reg, imm):
-	reg._state = b’00000000’ + imm
+	reg._state = b"00000000" + imm
+
 
 def mov_low2(reg, imm):
-	reg._state = b’00000000’ + imm
+	reg._state = b"00000000" + imm
+
 
 def mov_high1(reg, imm):
     reg._state = imm + reg[8:]
 
+
 def mov_high2(reg, imm):
     reg._state = imm + reg[8:]
+
 
 def mov(reg1, reg2):
     reg1._state = reg2._state
 
+
 def push(reg):
     pass
+
 
 def pop(reg):
     pass
 
+
 def add(reg1, reg2, reg3):
     reg1._state = reg2._state + reg3._state
 
-fucnctions_dictionary = {"load":load(), "mov_low1":mov_low1(), "mov_low2":mov_low2(), "mov_high1":mov_high1(), "mov_high2":mov_high2(), "mov":mov()}
+
+# def
+
+
+fucnctions_dictionary = {"load":load(), "mov_low1":mov_low1(),
+                        "mov_low2":mov_low2(), "mov_high1":mov_high1(),
+                        "mov_high2":mov_high2(), "mov":mov(), }
