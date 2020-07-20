242c9d378c43ae4fb9f5269b92215bb45e42dc23
Daria Omelkina <odasha2001@gmail.com>
2020-07-20 14:02:41 +0300

    changed functions behaviour: now they have different prarameters and return value

--- modules/functions.py
+++ modules/functions.py
@@ -19,100 +19,116 @@ logging.basicConfig(filename="../log.txt",
 logger = logging.getLogger('funclogger')
 
 
-def load(operands):
+def load_store(operands, flag_register):
     """
     Loads value from memory to register
-    Zero operand is register (destination)
-    First is its value
-    Second is the value to be stored
+    /
+    Stores value from register in the memory
+
+    Zero operand is the value of destination
+    First one is the value to be stored
 
     :param operands: list of operands
-    :return: NoneType
+    :param flag_register: Flag register
+    :return: value of the register/memory
     """
-    operands[0]._state = operands[2]
+    return operands[1]
 
 
-def store(operands):
-    """
-    Returns value, which later will be stored in the memory by CPU
-    Zero operand is register with memory location
-    First is its value
-    Second is the value to be stored
-
-    :param operands: list of operands
-    :return: bitarray
-    """
-    return operands[2]
-
-
-def mov_low(operands):
+def mov_low(operands, flag_register):
     """
     Writes immediate constant into the low byte of the register,
     setting high byte to all zeros
 
-    Zero operand is a register object, for writing information into it
-    First one is the value of that register
-    Second one is the value of the immediate constant
+    Zero operand is the value of the register object, which will be the destination
+    for writing information into it
+    First one is the value of the immediate constant
 
     :param operands: list of operands
-    :return: NoneType
+    :param flag_register: Flag register
+    :return: new value of the register
     """
-    operands[0]._state = bitarray("0" * 8) + operands[2]
+    return bitarray("0" * 8) + operands[1]
 
 
-def mov_high(operands):
+def mov_high(operands, flag_register):
     """
     Writes immediate constant into the high byte of the register,
     does not affect the low byte
 
-    Zero operand is a register object, for writing information into it
-    First one is the value of that register
-    Second one is the value of the immediate constant
+    Zero operand is the value of the register object, which will be the destination
+    for writing information into it
+    First one is the value of the immediate constant
 
     :param operands: list of operands
-    :return: NoneType
+    :param flag_register: Flag register
+    :return: new value of the register
     """
-    operands[0]._state[:8] = operands[2]
+    return operands[0][:8] + operands[1]
 
 
-def mov(operands):
+def mov(operands, flag_register):
     """
     Writes register value into another register
 
-    Zero operand is a register object, for writing information into it
-    First one is the value of that register
-    Second one is the value of the second register, which will be written
+    Zero operand is the value of the register object, which will be the destination
+    for writing information into it
+    First one is the value of the second register, which will be written
     into the first one
 
     :param operands: list of operands
-    :return: NoneType
+    :param flag_register: Flag register
+    :return: new value of the register
     """
-    operands[0]._state = bitarray(operands[2].to01())
+    return bitarray(operands[1].to01())
 
 
-def push(operands):
-    pass
+def push(operands, flag_register):
+    """
+    Pushes information from the register
+    into the memory stack
+
+    Zero operand is the value of that register
+
+    :param operands: list of operands (has only one value)
+    :param flag_register: Flag register
+    :return: value of the register
+    """
+    # TODO: return the correct value
+    #  How will register value be pushed onto the memory stack?
+    return bitarray(operands[0].to01())
 
 
-def pop(operands):
-    pass
+def pop(operands, flag_register):
+    """
+    Pops information from the top of the memory stack
+    and pushes it into the register
+
+    Zero operand is the value of that register
+
+    :param operands: list of operands (has only one value)
+    :return: ????
+    """
+    # TODO: return the correct value
+    #  How do I pop the value from the pop of the stack to return it, and to write it into the register?
+    return "?"
 
 
-def add(operands):
+def add(operands, flag_register):
     """
     Performs addition of two registers, saving the result in the third one
 
-    Zero operand is a the first register object, for writing information into it
-    First one is the value of that register
-    Second one is the value of the second register (first operand in addition)
-    Third one is the value of the third register (second operand in addition)
-    Forth one os the value of Flag register
+    Zero operand the value of the first register
+    First one is the value of the second register (first operand in the operation)
+    Second one is the value of the third register (second operand in the operation)
+    Third one is the value of Flag register
 
     :param operands: list of operands
-    :return: NoneType
+    :param flag_register: Flag register
+    :return: new value of the first register
     """
-    reg1 = twos_complement(int(operands[2].to01(), 2), len(operands[2]))
-    reg2 = twos_complement(int(operands[3].to01(), 2), len(operands[3]))
+    reg1 = twos_complement(int(operands[1].to01(), 2), len(operands[1]))
+    reg2 = twos_complement(int(operands[2].to01(), 2), len(operands[2]))
     result = bin(twos_complement(reg1 + reg2, len(operands[1])))[2:]
 
     result = result.rjust(16, "0")
@@ -122,161 +138,226 @@ def add(operands):
     #  finish remaining functions
 
     if len(result) > 16:
-        operands[4]._state[12] = "1"  # Carry flag
+        flag_register._state[12] = "1"  # Carry flag
         result = bin(twos_complement(reg1 + reg2, 18))[-16:]
-    elif operands[2].to01()[0] == operands[2].to01()[0] != result[0]:
-        operands[4]._state[14] = "1"  # Overflow flag
+    elif operands[1].to01()[0] == operands[2].to01()[0] != result[0]:
+        flag_register._state[14] = "1"  # Overflow flag
     elif result == "0":
         result = "0" * 16
-        operands[4]._state[13] = "1"  # Zero flag
+        flag_register._state[13] = "1"  # Zero flag
 
-    operands[0]._state = bitarray(result)
+    return bitarray(result)
 
 
-def sub(operands):
+def sub(operands, flag_register):
     """
     Performs subtraction of two registers, saving the result in the third one
 
-    Zero operand is a the first register object, for writing information into it
-    First one is the value of that register
-    Second one is the value of the second register (first operand in subtraction)
-    Third one is the value of the third register (second operand in subtraction)
-    Forth one os the value of Flag register
+    Zero operand the value of the first register
+    First one is the value of the second register (first operand in the operation)
+    Second one is the value of the third register (second operand in the operation)
+    Third one is the value of Flag register
 
     :param operands: list of operands
-    :return: NoneType
+    :param flag_register: Flag register
+    :return: new value of the first register
     """
-    reg1 = int(operands[2].to01(), 2)
-    reg2 = int(operands[3].to01(), 2)
-    result = str(bin(reg1 - reg2))[2:]
+    reg1 = twos_complement(int(operands[1].to01(), 2), len(operands[1]))
+    reg2 = twos_complement(int(operands[2].to01(), 2), len(operands[2]))
+    result = bin(twos_complement(reg1 - reg2, len(operands[1])))[2:]
+
+    result = result.rjust(16, "0")
 
     if len(result) > 16:
-        operands[4]._state[12] = "1"  # Carry flag
-    elif len(result) == 16:
-        operands[4]._state[14] = "1"  # Overflow flag
+        flag_register._state[12] = "1"  # Carry flag
+        result = bin(twos_complement(reg1 + reg2, 18))[-16:]
+    elif operands[1].to01()[0] == operands[2].to01()[0] != result[0]:
+        flag_register._state[14] = "1"  # Overflow flag
     elif result == "0":
         result = "0" * 16
-        operands[4]._state[13] = "1"  # Zero flag
-    else:
-        result = result.rjust(16, "0")
+        flag_register._state[13] = "1"  # Zero flag
 
-    operands[0]._state = bitarray(str(bin(result))[2:])
+    return bitarray(result)
 
 
-def mul(operands):
+def mul(operands, flag_register):
     """
     Performs multiplication of two registers, saving the result in the third one
 
-    Zero operand is a the first register object, for writing information into it
-    First one is the value of that register
-    Second one is the value of the second register (first operand in multiplication)
-    Third one is the value of the third register (second operand in multiplication)
-    Forth one os the value of Flag register
+    Zero operand the value of the first register
+    First one is the value of the second register (first operand in the operation)
+    Second one is the value of the third register (second operand in the operation)
+    Third one is the value of Flag register
 
     :param operands: list of operands
-    :return: NoneType
+    :param flag_register: Flag register
+    :return: new value of the first register
     """
-    reg1 = int(operands[2].to01(), 2)
-    reg2 = int(operands[3].to01(), 2)
-    result = str(bin(reg1 * reg2))[2:]
+    reg1 = twos_complement(int(operands[1].to01(), 2), len(operands[1]))
+    reg2 = twos_complement(int(operands[2].to01(), 2), len(operands[2]))
+    result = bin(twos_complement(reg1 * reg2, len(operands[1])))[2:]
+
+    result = result.rjust(16, "0")
 
     if len(result) > 16:
-        operands[4]._state[12] = "1"  # Carry flag
-        result = result[-16:]
-    elif len(result) == 16:
-        operands[4]._state[14] = "1"  # Overflow flag
+        flag_register._state[12] = "1"  # Carry flag
+        result = bin(twos_complement(reg1 + reg2, 18))[-16:]
+    elif operands[1].to01()[0] == operands[2].to01()[0] != result[0]:
+        flag_register._state[14] = "1"  # Overflow flag
     elif result == "0":
         result = "0" * 16
-        operands[4]._state[13] = "1"  # Zero flag
-    else:
-        result = result.rjust(16, "0")
+        flag_register._state[13] = "1"  # Zero flag
 
-    operands[0]._state = bitarray(result)
+    return bitarray(result)
 
 
-def div(operands):
+def div(operands, flag_register):
     """
     Performs division of two registers, saving the result in the third one
 
-    Zero operand is a the first register object, for writing information into it
-    First one is the value of that register
-    Second one is the value of the second register (first operand in division)
-    Third one is the value of the third register (second operand in division)
-    Forth one os the value of Flag register
+    Zero operand the value of the first register
+    First one is the value of the second register (first operand in the operation)
+    Second one is the value of the third register (second operand in the operation)
+    Third one is the value of Flag register
 
     :param operands: list of operands
-    :return: NoneType
+    :param flag_register: Flag register
+    :return: new value of the first register
     """
-    reg1 = int(operands[2].to01(), 2)
-    reg2 = int(operands[3].to01(), 2)
-    result = str(bin(reg1 // reg2))[2:]
+    reg1 = twos_complement(int(operands[1].to01(), 2), len(operands[1]))
+    reg2 = twos_complement(int(operands[2].to01(), 2), len(operands[2]))
+    result = bin(twos_complement(reg1 // reg2, len(operands[1])))[2:]
+
+    result = result.rjust(16, "0")
 
     if len(result) > 16:
-        operands[4]._state[12] = "1"  # Carry flag
-        result = result[-16:]
-    elif len(result) == 16:
-        operands[4]._state[14] = "1"  # Overflow flag
+        flag_register._state[12] = "1"  # Carry flag
+        result = bin(twos_complement(reg1 + reg2, 18))[-16:]
+    elif operands[1].to01()[0] == operands[2].to01()[0] != result[0]:
+        flag_register._state[14] = "1"  # Overflow flag
     elif result == "0":
         result = "0" * 16
-        operands[4]._state[13] = "1"  # Zero flag
-    else:
-        result = result.rjust(16, "0")
+        flag_register._state[13] = "1"  # Zero flag
 
-    operands[0]._state = bitarray(result)
+    return bitarray(result)
 
 
-def bit_and(operands):
-    reg1 = int(operands[2].to01(), 2)
-    reg2 = int(operands[3].to01(), 2)
-    result = str(bin(reg1 & reg2))[2:]
+def bit_and(operands, flag_register):
+    """
+    Performs bitwise and on two registers, saving the result in the third one
 
-    if result == "0":
-        operands[4]._state[13] = "1"  # Zero flag
+    Zero operand the value of the first register
+    First one is the value of the second register (first operand in the operation)
+    Second one is the value of the third register (second operand in the operation)
+    Third one is the value of Flag register
 
-    while len(result) != 16:
-        result = "0" + result
+    :param operands: list of operands
+     :param flag_register: Flag register
+   :return: new value of the first register
+    """
+    reg1 = twos_complement(int(operands[1].to01(), 2), len(operands[1]))
+    reg2 = twos_complement(int(operands[2].to01(), 2), len(operands[2]))
+    result = bin(twos_complement(reg1 & reg2, len(operands[1])))[2:]
 
-    operands[0]._state = bitarray(result)
+    result = result.rjust(16, "0")
 
+    if len(result) > 16:
+        flag_register._state[12] = "1"  # Carry flag
+        result = bin(twos_complement(reg1 + reg2, 18))[-16:]
+    elif operands[1].to01()[0] == operands[2].to01()[0] != result[0]:
+        flag_register._state[14] = "1"  # Overflow flag
+    elif result == "0":
+        result = "0" * 16
+        flag_register._state[13] = "1"  # Zero flag
 
-def bit_or(operands):
-    reg1 = int(operands[2].to01(), 2)
-    reg2 = int(operands[3].to01(), 2)
-    result = str(bin(reg1 | reg2))[2:]
-
-    if result == "0":
-        operands[4]._state[13] = "1"  # Zero flag
-
-    while len(result) != 16:
-        result = "0" + result
-
-    operands[0]._state = bitarray(result)
-
-
-def bit_xor(operands):
-    reg1 = int(operands[2].to01(), 2)
-    reg2 = int(operands[3].to01(), 2)
-    result = str(bin(reg1 ^ reg2))[2:]
-
-    if result == "0":
-        operands[4]._state[13] = "1"  # Zero flag
-
-    while len(result) != 16:
-        result = "0" + result
-
-    operands[0]._state = bitarray(result)
+    return bitarray(result)
 
 
-def bit_not(operands):
+def bit_or(operands, flag_register):
+    """
+    Performs bitwise or on two registers, saving the result in the third one
+
+    Zero operand the value of the first register
+    First one is the value of the second register (first operand in the operation)
+    Second one is the value of the third register (second operand in the operation)
+    Third one is the value of Flag register
+
+    :param operands: list of operands
+     :param flag_register: Flag register
+   :return: new value of the first register
+    """
+    reg1 = twos_complement(int(operands[1].to01(), 2), len(operands[1]))
+    reg2 = twos_complement(int(operands[2].to01(), 2), len(operands[2]))
+    result = bin(twos_complement(reg1 | reg2, len(operands[1])))[2:]
+
+    result = result.rjust(16, "0")
+
+    if len(result) > 16:
+        flag_register._state[12] = "1"  # Carry flag
+        result = bin(twos_complement(reg1 + reg2, 18))[-16:]
+    elif operands[1].to01()[0] == operands[2].to01()[0] != result[0]:
+        flag_register._state[14] = "1"  # Overflow flag
+    elif result == "0":
+        result = "0" * 16
+        flag_register._state[13] = "1"  # Zero flag
+
+    return bitarray(result)
+
+
+def bit_xor(operands, flag_register):
+    """
+    Performs bitwise xor on two registers, saving the result in the third one
+
+    Zero operand the value of the first register
+    First one is the value of the second register (first operand in the operation)
+    Second one is the value of the third register (second operand in the operation)
+    Third one is the value of Flag register
+
+    :param operands: list of operands
+    :param flag_register: Flag register
+    :return: new value of the first register
+    """
+    reg1 = twos_complement(int(operands[1].to01(), 2), len(operands[1]))
+    reg2 = twos_complement(int(operands[2].to01(), 2), len(operands[2]))
+    result = bin(twos_complement(reg1 ^ reg2, len(operands[1])))[2:]
+
+    result = result.rjust(16, "0")
+
+    if len(result) > 16:
+        flag_register._state[12] = "1"  # Carry flag
+        result = bin(twos_complement(reg1 + reg2, 18))[-16:]
+    elif operands[1].to01()[0] == operands[2].to01()[0] != result[0]:
+        flag_register._state[14] = "1"  # Overflow flag
+    elif result == "0":
+        result = "0" * 16
+        flag_register._state[13] = "1"  # Zero flag
+
+    return bitarray(result)
+
+
+def bit_not(operands, flag_register):
+    """
+    Performs bitwise and on two registers, saving the result in the third one
+
+    Zero operand the value of the first register
+    First one is the value of the second register (main operand in the operation)
+    Second one is the value of Flag register
+
+    :param operands: list of operands
+    :param flag_register: Flag register
+    :return: new value of the first register
+    """
     result = operands[1].to01()
     result.replace("1", "2")
     result.replace("0", "1")
     result.replace("2", "0")
 
     if result == "0" * 16:
-        operands[4]._state[13] = "1"  # Zero flag
+        flag_register._state[13] = "1"  # Zero flag
+    elif result[0] != operands[1][0]:
+        flag_register._state[14] = "1"  # Overflow flag
 
-    operands[0]._state = bitarray(result)
+    return bitarray(result)
 
 
 #
@@ -331,10 +412,10 @@ def twos_complement(val, bits):
     return val
 
 
-functions_dictionary = {"load": load, "mov_low": mov_low,
+functions_dictionary = {"load": load_store, "mov_low": mov_low,
                         "mov_high": mov_high, "mov": mov,
                         "push": push, "pop": pop, "add": add,
                         "sub": sub, "mul": push, "div": div,
                         "and": bit_and, "or": bit_or,
                         "xor": bit_xor, "not": bit_not,
-                        "store": store}
+                        "store": load_store}
