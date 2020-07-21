244b72905ae9c562ab01250ad4254210f0303b78
Daria Omelkina <odasha2001@gmail.com>
2020-07-21 14:48:07 +0300

    Slight changes

--- modules/functions.py
+++ modules/functions.py
@@ -24,10 +24,8 @@ def load_store(operands, flag_register):
     Loads value from memory to register
     /
     Stores value from register in the memory
-
     Zero operand is the value of destination
     First one is the value to be stored
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: value of the register/memory
@@ -39,11 +37,9 @@ def mov_low(operands, flag_register):
     """
     Writes immediate constant into the low byte of the register,
     setting high byte to all zeros
-
     Zero operand is the value of the register object, which will be the destination
     for writing information into it
     First one is the value of the immediate constant
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the register
@@ -55,11 +51,9 @@ def mov_high(operands, flag_register):
     """
     Writes immediate constant into the high byte of the register,
     does not affect the low byte
-
     Zero operand is the value of the register object, which will be the destination
     for writing information into it
     First one is the value of the immediate constant
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the register
@@ -70,12 +64,10 @@ def mov_high(operands, flag_register):
 def mov(operands, flag_register):
     """
     Writes register value into another register
-
     Zero operand is the value of the register object, which will be the destination
     for writing information into it
     First one is the value of the second register, which will be written
     into the first one
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the register
@@ -86,12 +78,10 @@ def mov(operands, flag_register):
 def add(operands, flag_register):
     """
     Performs addition of two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (first operand in the operation)
     Second one is the value of the third register (second operand in the operation)
     Third one is the value of Flag register
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the first register
@@ -119,12 +109,10 @@ def add(operands, flag_register):
 def sub(operands, flag_register):
     """
     Performs subtraction of two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (first operand in the operation)
     Second one is the value of the third register (second operand in the operation)
     Third one is the value of Flag register
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the first register
@@ -152,12 +140,10 @@ def sub(operands, flag_register):
 def mul(operands, flag_register):
     """
     Performs multiplication of two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (first operand in the operation)
     Second one is the value of the third register (second operand in the operation)
     Third one is the value of Flag register
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the first register
@@ -185,12 +171,10 @@ def mul(operands, flag_register):
 def div(operands, flag_register):
     """
     Performs division of two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (first operand in the operation)
     Second one is the value of the third register (second operand in the operation)
     Third one is the value of Flag register
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the first register
@@ -218,12 +202,10 @@ def div(operands, flag_register):
 def bit_and(operands, flag_register):
     """
     Performs bitwise and on two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (first operand in the operation)
     Second one is the value of the third register (second operand in the operation)
     Third one is the value of Flag register
-
     :param operands: list of operands
      :param flag_register: Flag register
    :return: new value of the first register
@@ -246,12 +228,10 @@ def bit_and(operands, flag_register):
 def bit_or(operands, flag_register):
     """
     Performs bitwise or on two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (first operand in the operation)
     Second one is the value of the third register (second operand in the operation)
     Third one is the value of Flag register
-
     :param operands: list of operands
      :param flag_register: Flag register
    :return: new value of the first register
@@ -274,12 +254,10 @@ def bit_or(operands, flag_register):
 def bit_xor(operands, flag_register):
     """
     Performs bitwise xor on two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (first operand in the operation)
     Second one is the value of the third register (second operand in the operation)
     Third one is the value of Flag register
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the first register
@@ -302,11 +280,9 @@ def bit_xor(operands, flag_register):
 def bit_not(operands, flag_register):
     """
     Performs bitwise and on two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (main operand in the operation)
     Second one is the value of Flag register
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the first register
@@ -331,12 +307,10 @@ def bit_not(operands, flag_register):
 def lsh(operands, flag_register):
     """
     Performs bitwise xor on two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (first operand in the operation)
     Second one is the value of the third register (second operand in the operation)
     Third one is the value of Flag register
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the first register
@@ -364,12 +338,10 @@ def lsh(operands, flag_register):
 def rsh(operands, flag_register):
     """
     Performs bitwise xor on two registers, saving the result in the third one
-
     Zero operand the value of the first register
     First one is the value of the second register (first operand in the operation)
     Second one is the value of the third register (second operand in the operation)
     Third one is the value of Flag register
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the first register
@@ -398,7 +370,6 @@ def cmp(operands, flag_register):
     """
     Compares two registers by subtraction of their values
     Only affects flags
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the flag register
@@ -427,7 +398,6 @@ def test(operands, flag_register):
     """
     Compares two registers by performing bitwise "and" on their values
     Only affects flags
-
     :param operands: list of operands
     :param flag_register: Flag register
     :return: new value of the flag register
