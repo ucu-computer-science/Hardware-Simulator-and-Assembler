a7ee5dc4873c06739b64316eceeec89a3671c02e
LastGenius-edu <as.andreysultanov@gmail.com>
2020-07-22 11:28:53 +0300

    Create a new test program for RISC-Accumualator ISA and update the instructions

---
+++ modules/program_examples/acc_test1.asm
@@ -0,0 +1,50 @@
+load
+loadf
+loadi
+load $5
+store
+storef
+storei
+store $5
+mov $5
+push
+pushf
+pushi
+pop
+popf
+popi
+add
+sub
+inc
+dec
+mul
+div
+and
+or
+xor
+not
+lsh $5
+rsh $5
+call $5
+call
+ret
+cmp
+cmp $5
+test
+test $5
+jmp $5
+jmp
+je $5
+je
+jne $5
+jne
+jg $5
+jg
+jge $5
+jge
+jl $5
+jl
+jle $5
+jle
+in $5
+out $5
