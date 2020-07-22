8a6aca006ef68d249b94666989b5a80b8473d668
Daria Omelkina <odasha2001@gmail.com>
2020-07-22 16:53:20 +0300

    Added input performance.

--- website/app.py
+++ website/app.py
@@ -3,10 +3,11 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from bitarray.util import ba2hex
 
 from modules.simulator import CPU
+from modules.assembler import Assembler, AssemblerError
 
 # COLOR PALETTE
 # TABLES
@@ -16,24 +17,66 @@ table_main_font_color = '#93B6D5'
 table_header_font_color = '#93B6D5'
 # BUTTONS
 button_color = '#46547C'
-button_font_color = '#EAB646'
+button_font_color = '#FCD848'
 # OTHER
 background_color = '#26273D'
-# TRANSPARENT LAYOUT FOR FIGURES
-
-
 # TRANSPARENT LAYOUT FOR FIGURES
 layout = go.Layout(
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)'
 )
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', 'url(assets/reset.css)']
 
-with open("modules/program_examples/assembly_test6.bin", "r") as file:
-    data = file.read()
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div([html.Div([
+    dcc.Textarea(id="input1", style={'display': 'inline-block'}, ),
+    html.Div(id='assembly', style={'display': 'inline-block'}, ),
+    html.Div(id='simulator', style={'display': 'inline-block'}, ),
+]),
+    html.Div([html.Div([
+        html.Button('Assemble', id='assemble_input', n_clicks=0,
+                    style={"color": button_font_color, "background-color": button_color}),
+    ], style={'bottom': '0', 'right': '0', 'left': '0',
+              'top': '0', 'display': 'inline-block'}),
+        html.Button('Next', id='next-instruction', n_clicks=0,
+                    style={"color": button_font_color, "background-color": button_color}),
+    ], style={'bottom': '0', 'right': '0', 'left': '0',
+              'top': '0', 'display': 'inline-block'})
+])
 
-cpu = CPU("risc3", "neumann", "special", data)
+
+@app.callback(Output('simulator', 'children'),
+              [Input('next-instruction', 'n_clicks')])
+def update_tables(n_clicks):
+    cpu.web_next_instruction()
+    time.sleep(0.05)
+    return html.Div([
+        html.Div(dcc.Graph(figure=make_instruction_slot(), config={
+            'displayModeBar': False, 'staticPlot': True}), style={'display': 'inline-block'}, ),
+        html.Div(dcc.Graph(figure=make_registers_slots(), config={
+            'displayModeBar': False, 'staticPlot': True}), style={'display': 'inline-block'}, ),
+        html.Div(dcc.Graph(figure=make_output_slot(), config={
+            'displayModeBar': False, 'staticPlot': True}), style={'display': 'inline-block'}, ),
+        html.Div(dcc.Graph(figure=make_memory_slots(), config={
+            'displayModeBar': False})),
+    ])
+
+
+@app.callback(Output('assembly', 'children'),
+              [Input('assemble_input', 'n_clicks')],
+              [State('input1', 'value')])
+def make_assembly_input(n_clicks, value):
+    global binary_program
+    global cpu
+    if value:
+        binary_program = Assembler("risc3", value).binary_code
+        cpu = CPU("risc3", "neumann", "special", binary_program)
+    else:
+        binary_program = ""
+    return binary_program
+
+binary_program = ''
+cpu = CPU("risc3", "neumann", "special", binary_program)
 
 
 def make_instruction_slot():
@@ -126,41 +169,20 @@ def make_memory_slots():
                                   fill_color=table_main_color,
                                   align=['left', 'center'],
                                   font=dict(color=table_main_font_color, size=12), ))], layout=layout)
-    fig.update_layout(width=1100, height=850, margin=dict(t=10, b=0),
-                      font=dict(family="Roboto Mono, monospace", color=table_main_font_color, size=20))
+    fig.update_layout(width=1100, height=400, margin=dict(t=10, b=0),
+                      font=dict(family="Roboto Mono, monospace", color=table_main_font_color, size=20),
+                      )
+    fig.layout.update(dragmode=False)
     return fig
-
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = html.Div([
-    html.Button('Next', id='submit-val', n_clicks=0,
-                style={"color": button_font_color, "background-color": button_color}),
-    html.Div(id='simulator')
-], style={'backgroundColor': background_color, 'bottom': '0', 'right': '0', 'left': '0',
-          'top': '0'})
-
-
-@app.callback(Output('simulator', 'children'),
-              [Input('submit-val', 'n_clicks')])
-def update_tables(n_clicks):
-    cpu.web_next_instruction()
-    time.sleep(0.05)
-    return html.Div([
-        html.Div(dcc.Graph(figure=make_instruction_slot(), config={
-            'displayModeBar': False}), style={'display': 'inline-block'}, ),
-        html.Div(dcc.Graph(figure=make_registers_slots(), config={
-            'displayModeBar': False}), style={'display': 'inline-block'}, ),
-        html.Div(dcc.Graph(figure=make_output_slot(), config={
-            'displayModeBar': False}), style={'display': 'inline-block'}, ),
-        html.Div(dcc.Graph(figure=make_memory_slots(), config={
-            'displayModeBar': False})),
-    ], style={'backgroundColor': background_color})
 
 
 server = app.server
 dev_server = app.run_server
 
 # run the program
+# TODO: make table undraggable (maybe switch to dash table)
+# TODO: Add error field
+
 if __name__ == '__main__':
     app.run_server(debug=True, threaded=True)
     # app.run_server(debug=True, processes=3, threaded=False)
