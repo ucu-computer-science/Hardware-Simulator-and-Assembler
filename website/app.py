cdc94a4f009362848094490924dd3f86a1d07586
Daria Omelkina <odasha2001@gmail.com>
2020-07-29 17:49:53 +0300

    Finally added table, which is not editable yet. It is not beautiful, but it might enable data editing in the simulator.

--- website/app.py
+++ website/app.py
@@ -12,6 +12,7 @@ import plotly.graph_objects as go
 from dash.dependencies import Input, Output, State
 from bitarray.util import ba2hex
 import uuid
+import dash_table
 
 # Imports from the project
 from modules.processor import CPU
@@ -123,14 +124,39 @@ app.layout = html.Div([
                                "background-color": button['background'],
                                'width': 160})
 
-        ]),
+        ], style={'display': 'inline-block'}),
 
         # Processor
-        html.Div([]),
+        html.Div([
+
+            html.Div([
+
+                # Next instruction
+                html.Div(id='instruction', style={'display': 'inline-block'}),
+
+                # Registers
+                html.Div(id='registers', style={'display': 'inline-block'}),
+
+                # Flags and output
+                html.Div([
+
+                    html.Div(id='flags', style={'display': 'inline-block'}),
+
+                    html.Div(id='output', style={'display': 'inline-block'}),
+
+                ], style={'display': 'inline-block'}),
+
+            ]),
+
+            # Memory
+            html.Div(id='memory'),
+
+        ], style={'display': 'inline-block'}),
 
     ]),
 
     # HIDDEN DIVS
+
     # Main info (has default settings)
     html.Div(id="info", children='risc3 neumann special', style={'display': 'none'}),
     # Id creation and storage
@@ -138,11 +164,23 @@ app.layout = html.Div([
     html.Div(id='id-creation', style={'display': 'none'}),
     # Binary and hexadecimal code translations storage
     html.Div(id='code', children=['', ''], style={'display': 'none'}),
-
+    # Instruction storage
+    html.Div(id='instruction-storage', children='0' * 16, style={'display': 'none'}),
+    # Memory storage (in a list, because Harvard architecture has two separate memories)
+    html.Div(id='memory-storage', children=['\n'.join(['\t'.join(['00 00 00 00'] * 32)] * 8), ''],
+             style={'display': 'none'}),
+    # Registers storage (first element –– registers, second –– their values )
+    html.Div(id='registers-storage',
+             children=[' '.join(['SP', 'IP', 'LR', 'FR', 'R00', 'R01', 'R02', 'R03']), ' '.join(['0000'] * 8)],
+             style={'display': 'none'}),
+    # Flags storage
+    html.Div(id='flags-storage', children=['0'] * 4, style={'display': 'none'}),
+    # Output storage
+    html.Div(id='output-storage', children='', style={'display': 'none'}),
 ])
 
 
-# APP CALLBACKS
+# APP CALLBACKS FOR INPUT/OUTPUT OF THE INFORMATION, ASSEMBLER
 # Change main info
 @app.callback(
     Output('info', 'children'),
@@ -151,7 +189,13 @@ app.layout = html.Div([
      Input('io-dropdown', 'value')])
 def update_output(isa, arch, io):
     """
+    Update main information about the cpu,
+    depending on the choice from dropdowns.
 
+    :param isa: chosen isa
+    :param arch: chosen architecture
+    :param io: chosen I/O mode
+    :return: string with information
     """
     return ' '.join([isa, arch, io])
 
@@ -161,6 +205,7 @@ def update_output(isa, arch, io):
 def get_ip(value):
     """
     Return randomly generated id each time new session starts
+
     :param value: is not used (is here by default)
     :return: random id
     """
@@ -175,6 +220,15 @@ def get_ip(value):
                Input('id-storage', 'children')],
               [State('input1', 'value')])
 def assemble(n_clicks, info, user_id, assembly_code):
+    """
+    Translate input assembly code to binary and hexadecimal ones.
+
+    :param n_clicks: is not used (is here by default)
+    :param info: isa, architecture and I/O mode
+    :param user_id: id of the session/user
+    :param assembly_code: input assembly code
+    :return: binary and hexadecimal codes or assembler error
+    """
     isa, architecture, io = info.split()
 
     global user_dict
@@ -202,7 +256,11 @@ def assemble(n_clicks, info, user_id, assembly_code):
                Input('code', 'children')])
 def render_content_hex_bin(tab, code_lst):
     """
+    Render two tabs: with binary and with hexadecimal code translations
 
+    :param tab: one of two: binary or hexadecimal
+    :param code_lst: list with binary and with hexadecimal code translations
+    :return: tabs
     """
     if tab == 'binary':
         return html.Div([
@@ -227,8 +285,99 @@ def render_content_hex_bin(tab, code_lst):
         ])
 
 
-# CREATE GRAPHIC ELEMENTS OF THE PROCESSOR
+# UPDATE HIDDEN INFO FOR PROCESSOR
 
+# APP CALLBACKS FOR CREATION OF GRAPHIC ELEMENTS OF THE PROCESSOR
+@app.callback(Output('instruction', 'children'),
+              [Input('instruction-storage', 'children')])
+def create_instruction(value):
+    """
+    # TODO
+    :param value:
+    :return:
+    """
+    return dcc.Textarea(value=value, disabled=True)
+
+
+@app.callback(Output('registers', 'children'),
+              [Input('registers-storage', 'children')])
+def create_registers(value):
+    """
+    # TODO
+    :param value:
+    :return:
+    """
+    return dcc.Textarea(value=str(value))
+
+
+@app.callback(Output('flags', 'children'),
+              [Input('flags-storage', 'children')])
+def create_flags(value):
+    """
+    # TODO
+    :param value:
+    :return:
+    """
+    return dcc.Textarea(value=str(value))
+
+
+@app.callback(Output('output', 'children'),
+              [Input('output-storage', 'children')])
+def create_output(value):
+    """
+    # TODO
+    :param value:
+    :return:
+    """
+    return dcc.Textarea(value=value, disabled=True)
+
+
+@app.callback(Output('memory', 'children'),
+              [Input('memory-storage', 'children')])
+def create_memory(value):
+    """
+    # TODO
+    :param value:
+    :return:
+    """
+    if not value[1]:
+        headers = ["Addr   :  "]
+        for i in range(0, 32, 4):
+            headers.append(f"{hex(i)[2:].rjust(2, '0')} {hex(i + 1)[2:].rjust(2, '0')} "
+                           f"{hex(i + 2)[2:].rjust(2, '0')} {hex(i + 3)[2:].rjust(2, '0')}")
+
+        rows = []
+        for i in range(0, 1024, 32):
+            rows.append(hex(i)[2:].rjust(8, "0"))
+
+        temp_lst1 = value[0].split('\n')
+        memory_data = []
+        for i in temp_lst1:
+            memory_data.append(i.split('\t'))
+
+        rows = [rows] + memory_data
+
+        data_lst = []
+        for y in range(len(rows[0])):
+            data_lst.append([])
+            for x in range(len(rows)):
+                data_lst[y].append(rows[x][y])
+
+        data = []
+        for x in range(len(rows[0])):
+            data.append(dict())
+            for y in range(len(rows)):
+                data[x][headers[y]] = data_lst[x][y]
+
+        return dash_table.DataTable(columns=([{'id': i, 'name': i} for i in headers]),
+                                    data=data,
+                                    style_table={'height': '300px', 'overflowY': 'auto'})
+
+
+# [{'Model': 1, 'Weight': 0, 'Torque': 0, 'Width': 0, 'Height': 0, 'Efficiency': 0, 'Power': 0, 'Displacement': 0},
+#  {'Model': 2, 'Weight': 0, 'Torque': 0, 'Width': 0, 'Height': 0, 'Efficiency': 0, 'Power': 0, 'Displacement': 0},
+#  {'Model': 3, 'Weight': 0, 'Torque': 0, 'Width': 0, 'Height': 0, 'Efficiency': 0, 'Power': 0, 'Displacement': 0},
+#  {'Model': 4, 'Weight': 0, 'Torque': 0, 'Width': 0, 'Height': 0, 'Efficiency': 0, 'Power': 0, 'Displacement': 0}]
 # Run the program
 if __name__ == '__main__':
     # SERVER LAUNCH
