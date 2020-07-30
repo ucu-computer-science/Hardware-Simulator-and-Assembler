67682f73757d8032429adfdeeba472beb35554ad
LastGenius-edu <as.andreysultanov@gmail.com>
2020-07-30 18:22:23 +0300

    Update the stack architecture demo program

--- modules/demos/risc1/alphabet_printout.asm
+++ modules/demos/risc1/alphabet_printout.asm
@@ -1,9 +1,22 @@
-mov $72
-mov $100
-mov $99
-mov $98
-mov $97
-cmpe $72
-jc $3
+# Load the starting ASCII value into TOS
+mov $65
+
+# Duplicate it for the comparison
+dup
+
+# Check if we have already finished printing the alphabet
+cmpe $91
+
+# Jump to the end of the program if we are finished with printing
+jc $6
+
+# Output TOS value to the device at port 1
+dup
 out $1
-jmp $-3
+
+# Increment the value of the ASCII code by 1
+mov $1
+add
+
+# Jump back to the start of the loop
+jmp $-7
