a0ca2c0c4ae2291e42d8a79985984c53748aaa8e
Daria Omelkina <odasha2001@gmail.com>
2020-08-28 15:07:57 +0300

    Start Risc2 polynomial.

--- modules/demos/cisc/bubble_sort.asm
+++ modules/demos/cisc/bubble_sort.asm
@@ -1,55 +1,84 @@
-# Bubble Sort | Register-CISC example Assembly program
+# Polynomial Calculation | Register-CISC example Assembly program
 
-# These instructions sort a manually 'created' list
-# Directive .start contains the starting index of the list (0 in that example)
-# Directive .end contains the ending index of the list (6 in that example)
-# Values must be manually written into the data memory after the program was assembled
-# Example list: 00 30 00 20 00 10 00 00
-# Result will be: 00 00 00 10 00 20 00 30
+# These instructions calculate a value of a polynomial expression
+# In that example polynomial is 2*x^2 + 3*x - 7, x = 2
+# Answer will be 7
+
+# Directive .start contains the starting index of the list with coefficients (2 in that example)
+# Directive .end contains the ending index of the list with coefficients (6 in that example)
+# Directive .n contains maximum power of the polynomial expression (2 in that example)
+# Directive .value contains maximum power of the variable (2 in that example)
+# Directive .resultindex contains address, where result will be stored (0 in that example)
+
+
+# Coefficients must be manually written into the data memory after the program was assembled
+# Example list of coefficients: 00 02 00 03 ff f9
 
 # Directives:
-.start db 0
+.start db 2
 .end db 6
+.n db 2
+.value db 2
+.resultindex db 0
 
-.listloop
-# R02 register contains current index of the list
-# R03 is used as changes indicator (bubble sort is finished when no changes are present)
-mov %R02, .start
-mov %R03, $0
+# Store initial info in registers
+mov %R03, .start
+mov %R02, .n
 
-.mainloop
-# Load two values into registers (step is two bytes, for it is the length of the word)
-mov %R00, [%R02]
-mov %R01, [%R02+$2]
 
-# Compare them
-cmp %R00, %R01
+# Evaluation loop:
+.evalloop
+# Store current info in registers
+mov %R00, $1
+mov %R01, %R02
 
-# If they are in the right order –– skip swapping them
-jle .continue
+.powerloop
+# If current power is less than 1, skip to multiplying by coefficient
+cmp %R01, $1
+jl .multcoef
 
-# Swapping
+# Raise value of x to the current power
+mul %R00, .value
+dec %R01
+jmp .powerloop
+
+.multcoef
+# Multiply by the coefficient
+mov %R01, [%R03]
+mul %R00, %R01
+
+# Push result into memory
 push %R00
-mov %R00, %R01
+
+# Check if we are finished with going through the coefficients list
+inc %R03
+inc %R03
+cmp %R03, .end
+jg .additionloop
+
+# If not, decrement power and continue going through the list
+dec %R02
+jmp .evalloop
+
+
+# Addition loop:
+.additionloop
+# Add all the evaluated values and store result in memory
+mov %R00, $0
+
+.addition
 pop %R01
+add %R00, %R01
+cmp %R03, .start
+je .storing
+dec %R03
+dec %R03
+jmp .addition
 
-# Changing values in the memory
-mov [%R02], %R00
-mov [%R02+$2], %R01
-
-# Set indicator to True
-mov %R03, $1
-
-# Check if we finished going through the list
-.continue
-mov %R00, $2
-add %R02, %R00
-cmp %R02, .end
-jne .mainloop
-
-# Check if anything has changed in the list: if it did, go through the list once again
-cmp %R03, $1
-je .listloop
+# Store result value
+.storing
+mov %R01, .resultindex
+mov [%R01], %R00
 
 # Finish the program
 nop
--- modules/demos/risc2/polynomial.asm
+++ modules/demos/risc2/polynomial.asm
@@ -1,3 +1,22 @@
 # Polynomial Calculation | Accumulator-RISC example Assembly program
 
-# Coming soon...
+# These instructions calculate a value of a polynomial expression
+# In that example polynomial is 2*x^2 + 3*x - 7, x = 2
+# Answer will be 7
+
+# Directive .start contains the starting index of the list with coefficients (2 in that example)
+# Directive .end contains the ending index of the list with coefficients (6 in that example)
+# Directive .n contains maximum power of the polynomial expression (2 in that example)
+# Directive .value contains maximum power of the variable (2 in that example)
+# Directive .resultindex contains address, where result will be stored (0 in that example)
+
+
+# Coefficients must be manually written into the data memory after the program was assembled
+# Example list of coefficients: 00 02 00 03 ff f9
+
+# Directives:
+.start db 2
+.end db 6
+.n db 2
+.value db 2
+.resultindex db 0
