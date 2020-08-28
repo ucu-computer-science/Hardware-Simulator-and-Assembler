f75785307cb4b550a9387bce794b57a3e311514d
Daria Omelkina <odasha2001@gmail.com>
2020-08-28 18:57:53 +0300

    Add polynomial example for Risc2.

--- modules/demos/risc2/polynomial.asm
+++ modules/demos/risc2/polynomial.asm
@@ -20,3 +20,71 @@
 .n db 2
 .value db 2
 .resultindex db 0
+
+# Load initial start index to IR
+mov .start
+storei
+
+# Push maximum power of the expression
+mov .n
+push
+push
+
+# Go through coefficients in the list
+.listloop
+loadi
+cmp .end
+jg .startaddition
+
+# Work with each coefficient
+.powerloop
+pop
+
+# Skip multiplication if power is less than 1
+cmp $1
+jl .finishpowerloop
+dec
+push
+mov .value
+mul
+store
+jmp .powerloop
+
+# Finish with raising value to the power
+.finishpowerloop
+pop
+dec
+push
+push
+loadi
+inc
+inc
+storei
+jmp .listloop
+
+# Add values in the list
+.startaddition
+mov $0
+push
+
+.additionloop
+loadi
+cmp .start
+je .result
+dec
+dec
+storei
+pop
+add
+push
+jmp .additionloop
+
+# Store the result in the memory
+.result
+mov .resultindex
+storei
+pop
+store
+
+# Finish the program
+nop
