e4efd15d8b970fb0ba3874dfb577007e9762f95a
Daria Omelkina <odasha2001@gmail.com>
2020-08-29 23:28:11 +0300

    Bubble sort for Risc1.

--- modules/demos/risc1/bubble_sort.asm
+++ modules/demos/risc1/bubble_sort.asm
@@ -1,43 +1,82 @@
 # Bubble Sort | Stack-RISC example Assembly program
 
-# NOT FINISHED!!!
-
 # These instructions sort a manually 'created' list
 # Directive .start contains the starting index of the list (0 in that example)
 # Directive .end contains the ending index of the list (6 in that example)
+# Directive .indicator contains the address, where changes indicator will be stored (10 in that example)
 # Values must be manually written into the data memory after the program was assembled
-# Example list: 00 30 00 20 00 10 00 00
+# Example list: 00 30 00 20 00 00 00 10
 # Result will be: 00 00 00 10 00 20 00 30
 
 # Directives:
 .start db 0
 .end db 6
+.indicator db 10
 
+.listloop
+mov $0
+push
+# Change indicator to False
+mov .indicator
+store $0
 # Add first index to stack
 mov .start
 
-.loadingloop
-# Add value from the list at that index
+.loop
+# Save index and load value from list
+dup
+push
 dup
 push
 load
-pop
 
-# Add next index to stack
+# Pop previous index, add 2 to it and load next value
+pop
 mov $2
 add
-
-# Check if the list has finished (in this example we use 3-bytes list)
 dup
+push
+load
+
+# Compare two values and swap them if needed
+cmpb
+jc .continue
+
+# Swap
+pop
+dup
+mov $-2
+add
+load
+pop
+dup
+mov $2
+add
+dup
+dup
+push
+push
+load
+store
+store
+# Change indicator to true
+mov .indicator
+store $1
+
+.continue
+pop
+dup
+push
 cmpe .end
-jc .swaploop
+jc .checkcondition
+pop
+jmp .loop
 
-# Continue going through the list
-jmp .loadingloop
+.checkcondition
+mov .indicator
+load
+cmpe $1
+jc .listloop
 
-.swaploop
-# Move to the start of the list
-push
-push
-push
-push
+# Finish the program
+nop
